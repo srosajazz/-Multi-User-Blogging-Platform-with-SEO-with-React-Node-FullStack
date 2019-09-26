6fce21de44d0a9ab6911b4ca01d9aa5b34368b06
Sergio Rosa <scarlosjazz@gmail.com>
2019-09-26 15:22:03 -0400

    Database setup with mongo atlas

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 // app
 const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected'));
 
 //middlewares
 app.use(morgan('dev'));
@@ -14,7 +25,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //cors
-app.use(cors());
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+  app.use(cors({ origin: `{process.env.CLIENT_URL}` }));
+}
 
 // routes
 app.get('/api', (req, res) => {
