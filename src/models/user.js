0bc8e13fee85822bf6f779e82496259ae3539d8b
Sergio Rosa <scarlosjazz@gmail.com>
2019-09-26 19:35:19 -0400

    User model and schema

---
+++ src/models/user.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    profile: {
+      type: String,
+      required: true,
+    },
+
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+
+    salt: String,
+    about: {
+      type: Number,
+      trim: true,
+    },
+
+    role: {
+      type: Number,
+      trim: true,
+    },
+
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+
+    resetPasswordLink: {
+      data: String,
+      default: '',
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model('User, userSchema');
